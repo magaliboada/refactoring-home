4aabe52e39d95939d2ccdc707df82601f8a13607
magali <magali@degustabox.com>
2021-01-02 11:56:37 +0100

    Add Apache to composer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,15 +3,9 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
-class AppFixtures extends Fixture
+class AppFixtures
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
 
-        $manager->flush();
-    }
 }
